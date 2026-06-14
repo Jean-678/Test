68b8d5d991881ca0441d13ef27a695df6299f949
Jean-678 <jeanbaptist.malcekan@univ.edu.vu>
2026-06-14 12:05:19 +1100

    update health endpoint to return status and environment information

--- api.js
+++ api.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const visiteur = process.env.visiteur || 'Anonyme';
 
-app.get('/', (req, res) => {
-  res.send(`Hello ${visiteur}! You have well deployed your app: Congratulations!`);
+app.get('/health', (req, res) => {
+  res.json({
+    status: "ok",
+    environment: process.env.APP_ENV || "local"
+  });
 });
 
 const PORT = process.env.PORT || 3000; 
